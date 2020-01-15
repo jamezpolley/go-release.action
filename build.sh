5b526f451b01aef3f99f010ee2e34241999fd0df
James Polley <jp@jamezpolley.com>
2020-01-15 15:52:47 +1100

    Allow for use of  BUILD_OPTS for optional extra build arguments

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ set -eux
 
 PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
 
+BUILD_OPTS="${BUILD_OPTS:-}"
+
 mkdir -p $PROJECT_ROOT
 rmdir $PROJECT_ROOT
 ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
 cd $PROJECT_ROOT
 go get -v ./...
-go build
+go build ${BUILD_OPTS}
